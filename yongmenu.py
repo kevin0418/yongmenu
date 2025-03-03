b8265e3bf83d337138ca7d74ae6a41481aebafbc
kevin0418 <54438552+kevin0418@users.noreply.github.com>
2025-03-03 21:30:28 +1000

    add first time
    add first time upload, Main menu

---
+++ yongmenu.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+st.write("# Property Sale by Kevin👋")
+
+with st.sidebar:
+     
+     st.markdown(
+      """
+          #### Propert listing
+      
+          - [Koorakbyn Resort  ](https://kevinoh-kresort-menu.streamlit.app/)
+          
+          - [Prawns Farm  ](https://kevinoh-prawnfarm-menu.streamlit.app/)
+          
+          - [Sport Complex  ](https://kevinohgraham.streamlit.app/)
+         
+          
+          
+                 
+    """
+    )
+     
+
+st.markdown(
+    """
+   ### Contact information 
+      
+   #### Yong Real Estate,     click below 
+
+
+  
+    [Sue Yi         : 0412 317 386](https://www.yong.com.au/Sue-Ye/)\n
+    [Kevin  Oh      : 0402 283 003](https://www.yong.com.au/Kevin-Oh/) 
+
+    
+"""
+)
